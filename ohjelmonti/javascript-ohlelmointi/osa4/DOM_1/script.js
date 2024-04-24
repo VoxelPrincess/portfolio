5f4ef7374a2a807270805dc559aa39588af4a9af
VoxelPrincess <anna.v.bekharskaya@gmail.com>
2024-04-24 21:25:09 +0300

    GitIgnore

--- ohjelmonti/javascript-ohlelmointi/osa4/DOM_1/script.js
+++ ohjelmonti/javascript-ohlelmointi/osa4/DOM_1/script.js
@@ -2,3 +2,19 @@
 Kirjoita JavaScript-koodi, joka valitsee sisällön tunnuksen käyttämällä getElementById-metodia.
 Kirjoita funktio, joka päivittää valitun elementin tekstisisällön painamalla nappia tekstiksi "Hei, maailma!".
 */
+
+/*
+Напишите код JavaScript, который выбирает идентификатор содержимого с помощью метода getElementById.
+Напишите функцию, которая обновляет текстовое содержимое выбранного элемента при нажатии кнопки на текст «Привет, мир!».
+*/
+
+document.getElementById("button").addEventListener("click", function () {
+  document.getElementById("content").textContent = "Hei maailma!";
+});
+
+// method 2   Laura metodi
+
+const changeText = () => {
+  const textPlacement = document.getElementById("content");
+  textPlacement.innerHTML = "HEI MAAILMA !";
+};
--- ohjelmonti/javascript-ohlelmointi/osa4/DOM_2/script.js
+++ ohjelmonti/javascript-ohlelmointi/osa4/DOM_2/script.js
@@ -8,3 +8,60 @@ Kun nappia klikataan, funktion tulisi:
 
 Varmista, että syötekenttä tyhjennetään hedelmän lisäämisen jälkeen.
 */
+
+/*
+Напишите код JavaScript, который добавляет к кнопке обработчик событий.
+
+При нажатии кнопки функция должна:
+- Получите значение из поля ввода.
+— Создайте новый элемент <li> и установите его текстовое содержимое в качестве значения поля ввода.
+— Добавьте новый элемент <li> в элемент <ul> списка фруктов.
+
+Убедитесь, что поле ввода очищено после добавления фруктов.
+*/
+
+/*
+
+const button = document.querySelector("#addFruitBtn");
+
+const addToList = () => {
+  const fruitInput = document.querySelector("#fruitInput").value;
+  const fruitList = document.querySelector("#fruitList");
+
+  if (fruitInput) {
+    const li = document.createElement("li");
+    li.textContent = fruitInput;
+    fruitList.appendChild(li);
+
+    fruitInput.value = " ";
+
+    console.log(fruitInput);
+  } else {
+    alert("Lisää hedelmän nimi , kiitos");
+  }
+  document.querySelector("#fruitInput").value = "";
+}; 
+
+// button.addEventListener("click", addToList);
+*/
+
+// method 2 laura versio
+
+const button = document.querySelector("#addFruitBtn");
+const addToList = () => {
+  const fruitInput = document.querySelector("#fruitInput");
+  const fruitList = document.querySelector("#fruitList");
+
+  if (fruitInput.value) {
+    const li = document.createElement("li");
+    li.textContent = fruitInput.value;
+    fruitList.appendChild(li);
+    fruitInput.value = "";
+    console.log(fruitInput);
+  } else {
+    alert("Lisää hedelmän nimi , kiitos");
+  }
+  document.querySelector("#fruitInput").value = "";
+};
+
+button.addEventListener("click", addToList);
